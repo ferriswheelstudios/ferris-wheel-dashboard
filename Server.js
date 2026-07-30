158856faa598b16a6cd854d5bff06b2f948e34e0
ferris wheel studios <amrish.fws@gmail.com>
2026-07-30 18:07:35 +0530

    add mac bridge support

--- Server.js
+++ Server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { exec } = require("child_process");
 
 const app = express();
 const server = http.createServer(app);
@@ -17,11 +16,25 @@ let dashboardData = {
     lyrics: "Paste Lyrics Here..."
 };
 
+// Registered Macs
+const macClients = new Set();
+
 io.on("connection", (socket) => {
 
     console.log("Device Connected");
 
     socket.emit("dashboardData", dashboardData);
+
+    // Register Mac bridge
+    socket.on("registerMac", () => {
+        macClients.add(socket);
+        console.log("Mac Connected");
+
+        socket.on("disconnect", () => {
+            macClients.delete(socket);
+            console.log("Mac Disconnected");
+        });
+    });
 
     socket.on("updateData", (data) => {
 
@@ -42,20 +55,173 @@ io.on("connection", (socket) => {
         io.emit("lyricsScroll", amount);
 
     });
+
     socket.on("showSessionView", () => {
+
         console.log("SHOW SESSION VIEW BUTTON PRESSED");
-    exec(
-        `osascript -e 'tell application "Ableton Live 12 Suite" to activate'`,
-        (err) => {
-            if (err) {
-                console.error(err);
-            }
-        }
-    );
-});
+
+        // Send command to all connected Macs
+        macClients.forEach((client) => {
+            client.emit("showSessionView");
+        });
+
+    });
 
 });
 
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+
+    console.log("Server Running on Port " + PORT);
+
+});const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let dashboardData = {
+    artist: "Artist Name",
+    song: "Song Name",
+    engineer: "Engineer",
+    sessionTime: "12:00 PM - 02:00 PM",
+    lyrics: "Paste Lyrics Here..."
+};
+
+// Registered Macs
+const macClients = new Set();
+
+io.on("connection", (socket) => {
+
+    console.log("Device Connected");
+
+    socket.emit("dashboardData", dashboardData);
+
+    // Register Mac bridge
+    socket.on("registerMac", () => {
+        macClients.add(socket);
+        console.log("Mac Connected");
+
+        socket.on("disconnect", () => {
+            macClients.delete(socket);
+            console.log("Mac Disconnected");
+        });
+    });
+
+    socket.on("updateData", (data) => {
+
+        dashboardData = {
+            artist: data.artist || "",
+            song: data.song || "",
+            engineer: data.engineer || "",
+            sessionTime: data.sessionTime || "",
+            lyrics: data.lyrics || ""
+        };
+
+        io.emit("dashboardData", dashboardData);
+
+    });
+
+    socket.on("lyricsScroll", (amount) => {
+
+        io.emit("lyricsScroll", amount);
+
+    });
+
+    socket.on("showSessionView", () => {
+
+        console.log("SHOW SESSION VIEW BUTTON PRESSED");
+
+        // Send command to all connected Macs
+        macClients.forEach((client) => {
+            client.emit("showSessionView");
+        });
+
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+
+    console.log("Server Running on Port " + PORT);
+
+});const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let dashboardData = {
+    artist: "Artist Name",
+    song: "Song Name",
+    engineer: "Engineer",
+    sessionTime: "12:00 PM - 02:00 PM",
+    lyrics: "Paste Lyrics Here..."
+};
+
+// Registered Macs
+const macClients = new Set();
+
+io.on("connection", (socket) => {
+
+    console.log("Device Connected");
+
+    socket.emit("dashboardData", dashboardData);
+
+    // Register Mac bridge
+    socket.on("registerMac", () => {
+        macClients.add(socket);
+        console.log("Mac Connected");
+
+        socket.on("disconnect", () => {
+            macClients.delete(socket);
+            console.log("Mac Disconnected");
+        });
+    });
+
+    socket.on("updateData", (data) => {
+
+        dashboardData = {
+            artist: data.artist || "",
+            song: data.song || "",
+            engineer: data.engineer || "",
+            sessionTime: data.sessionTime || "",
+            lyrics: data.lyrics || ""
+        };
+
+        io.emit("dashboardData", dashboardData);
+
+    });
+
+    socket.on("lyricsScroll", (amount) => {
+
+        io.emit("lyricsScroll", amount);
+
+    });
+
+    socket.on("showSessionView", () => {
+
+        console.log("SHOW SESSION VIEW BUTTON PRESSED");
+
+        // Send command to all connected Macs
+        macClients.forEach((client) => {
+            client.emit("showSessionView");
+        });
+
+    });
+
+});
 
 const PORT = process.env.PORT || 3000;
 
